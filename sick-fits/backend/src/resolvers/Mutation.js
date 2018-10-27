dd9a3c7e9f1fc0d377cecb66f7e94109ba61f523
Dragan <d.novakovic1810@yahoo.com>
2018-10-27 19:11:20 +0200

    prisma setup

--- sick-fits/backend/src/resolvers/Mutation.js
+++ sick-fits/backend/src/resolvers/Mutation.js
@@ -1,3 +1,17 @@
-const mutations = {};
+const Mutations = {
+  async createItem(parent, args, ctx, info) {
+    // TODO check if logged in
+    const item = await ctx.db.mutation.createItem(
+      {
+        data: {
+          ...args
+        }
+      },
+      info
+    );
 
-module.exports = mutations;
+    return item;
+  }
+};
+
+module.exports = Mutations;
--- sick-fits/backend/src/resolvers/Query.js
+++ sick-fits/backend/src/resolvers/Query.js
@@ -1,3 +1,11 @@
-const Query = {};
+const { forwardTo } = require("prisma-binding");
+
+const Query = {
+  items: forwardTo("db")
+  // async items(parent, args, ctx, info) {
+  //  const item = await ctx.db.query.items();
+  //  return item;
+  // }
+};
 
 module.exports = Query;

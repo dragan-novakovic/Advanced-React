cfb6c7f97da6355caab1969350e381beb37d7946
dragan1810 <d.novakovic1810@yahoo.com>
2019-01-14 15:19:09 +0100

    qqq

--- sick-fits-ts/frontend/pages/index.tsx
+++ sick-fits-ts/frontend/pages/index.tsx
@@ -1,3 +1,9 @@
-import React from 'react'
+import Link from 'next/link';
 
-export default () => <div>Ofjwofjowajfwoafjwaofjwao</div>
+const Home = props => (
+  <div>
+    <p>Hey!</p>
+  </div>
+);
+
+export default Home;

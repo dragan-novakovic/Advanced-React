c410f8901df397957162ccd91e13f8631ff5b99e
Dragan Novakovic <dragan.novakovic@bridgewaterlabs.com>
2019-02-19 20:27:57 +0100

    qqq

--- sick-fits-ts/backend/src/index.js
+++ sick-fits-ts/backend/src/index.js
@@ -1,1 +1,20 @@
-// let's go!
+require('dotenv').config();
+
+const createServer = require('./createSever');
+
+const server = createServer();
+
+// TODO use express middleware for JWT
+// TODO populate current user
+
+const serverOptions = {
+  cors: {
+    credentials: true,
+    origin: process.env.FRONTEND_URL
+  }
+};
+
+server.start(
+  serverOptions,
+  deets => void console.log(`Server is running on port: ${deets.port}`)
+);

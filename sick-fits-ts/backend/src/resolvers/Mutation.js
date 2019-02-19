5afa119b2f4d58dfb82c17f68839eb0e3f7b7954
Dragan Novakovic <dragan.novakovic@bridgewaterlabs.com>
2019-02-19 21:11:36 +0100

    14

--- sick-fits-ts/backend/src/resolvers/Mutation.js
+++ sick-fits-ts/backend/src/resolvers/Mutation.js
@@ -1,3 +1,11 @@
-const mutations = {};
+const mutations = {
+  async createItem(parent, args, ctx, info) {
+    // check if logged in
+
+    const item = await ctx.db.mutation.createItem({ data: { ...args } }, info);
+
+    return item;
+  }
+};
 
 module.exports = mutations;
--- sick-fits-ts/backend/src/resolvers/Query.js
+++ sick-fits-ts/backend/src/resolvers/Query.js
@@ -1,3 +1,14 @@
-const Query = {};
+const { forwardTo } = require('prisma-binding');
+
+const Query = {
+  items: forwardTo('db')
+  /*
+  async items(parent, args, ctx, info) {
+    const items = ctx.db.query.items();
+
+    return items;
+  }
+  */
+};
 
 module.exports = Query;

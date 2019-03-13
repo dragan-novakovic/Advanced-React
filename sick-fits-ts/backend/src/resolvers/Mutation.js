1f4a5506f51bdad0cdb90512a94d0acf0a951f05
Dragan Novakovic <dragan.novakovic@bridgewaterlabs.com>
2019-03-13 21:13:43 +0100

    qqqq

--- sick-fits-ts/backend/src/resolvers/Mutation.js
+++ sick-fits-ts/backend/src/resolvers/Mutation.js
@@ -283,6 +283,73 @@ const mutations = {
       },
       info
     );
+  },
+
+  async createOrder(parent, args, ctx, info) {
+    // 1. Query the current user that is signed in
+    const { userId } = ctx.request;
+    if (!userId) {
+      throw new Error('You must be signed in to complete the order');
+    }
+    const user = await ctx.db.query.user(
+      { where: { id: userId } },
+      `{
+        id 
+        name 
+        email 
+        cart { 
+          id 
+          quantity 
+          item {
+            title
+            price
+            id
+            description
+            image
+            largeImage
+          }
+        }
+      }`
+    );
+    // 2. recalculate the total price
+    const amount = user.cart.reduce(
+      (tally, cartItem) => tally + cartItem.item.price * cartItem.quantity,
+      0
+    );
+    // 3. create the stripe charge
+    const charge = await stripe.charges.create({
+      amount,
+      currency: 'USD',
+      source: args.token
+    });
+    // 4. convert cartItem to orderItem
+    const orderItems = user.cart.map(cartItem => {
+      const orderItem = {
+        ...cartItem.item,
+        quantity: cartItem.quantity,
+        user: { connect: { id: userId } }
+      };
+      delete orderItem.id;
+      return orderItem;
+    });
+
+    const order = await ctx.db.mutation.createOrder({
+      data: {
+        total: charge.amount,
+        charge: charge.id,
+        items: { create: orderItems },
+        user: { connect: { id: userId } }
+      }
+    });
+    // 5. clean up the cart
+    const cartItemsIds = user.cart.map(cartItem => cartItem.id);
+    await ctx.db.mutation.deleteManyCartItems({
+      where: {
+        id_in: cartItemsIds
+      }
+    });
+    // 6. return the Order to client
+    return order;
   }
 };
 
--- sick-fits-ts/frontend/components/Header.tsx
+++ sick-fits-ts/frontend/components/Header.tsx
@@ -5,6 +5,7 @@ import styled from 'styled-components';
 import NProgress from 'nprogress';
 
 import Cart from './Cart';
+import Search from './Search';
 
 Router.onRouteChangeStart = () => {
   NProgress.start();
@@ -71,7 +72,7 @@ const Header = () => (
       <Nav />
     </div>
     <div className="sub-bar">
-      <p>Search</p>
+      <Search />
     </div>
     <Cart />
   </StyledHeader>

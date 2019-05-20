7be13c8b1f9d89e29f171bed1d7ceb4b429b7e65
Dragan <dragan.novakovic@bridgewaterlabs.com>
2019-05-20 21:25:32 +0200

    qqq

--- frontend/lib/withData.js
+++ frontend/lib/withData.js
@@ -1,10 +1,10 @@
 import withApollo from 'next-with-apollo';
 import ApolloClient from 'apollo-boost';
-import { endpoint } from '../config';
+import { endpoint, real_endpoint } from '../config';
 
 function createClient({ headers }) {
   return new ApolloClient({
-    uri: process.env.NODE_ENV === 'development' ? endpoint : endpoint,
+    uri: process.env.NODE_ENV === 'development' ? endpoint : real_endpoint,
     request: async operation => {
       // check headers
       operation.setContext({

2f682b035f397ef33c5143a3b5432336b5312b7f
Dragan <d.novakovic1810@yahoo.com>
2019-05-20 21:36:43 +0200

    Update config.js

--- frontend/config.js
+++ frontend/config.js
@@ -1,4 +1,4 @@
 // This is client side config only - don't put anything in here that shouldn't be public!
-export const endpoint = 'http://localhost:4444';
+export const endpoint = 'http://46.101.103.143:4444';
 export const real_endpoint = 'http://46.101.103.143:4444';
 export const perPage = 4;
